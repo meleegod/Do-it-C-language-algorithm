e2bc40db259d5d6fc537077142d5279bde076a36
wotjd0963 <wotjd0963@naver.com>
2022-05-15 20:01:55 +0900

    Q2

---
+++ 1장/Q2_min3.c
@@ -0,0 +1,26 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a, b, c;
+//     int min;
+    
+//     printf("값을 입력하세요\n");
+//     printf("a의 값 :");
+//     scanf("%d",&a);
+//     printf("b의 값 :");
+//     scanf("%d",&b);
+//     printf("c의 값 :");
+//     scanf("%d",&c);
+
+//     min = a;
+//     if(b < min)
+//     {
+//         min = b;
+//     }
+//     if(c < min)
+//     {
+//         min = c;
+//     }
+//     printf("최솟값은 %d입니다",min);
+// }
