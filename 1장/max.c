0742498c82dfaefab8cb9a39315d06f71e33b223
wotjd0963 <wotjd0963@naver.com>
2022-05-15 19:58:28 +0900

    max

---
+++ 1장/max.c
@@ -0,0 +1,26 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int a,b,c;
+//     int max;
+
+//     printf("세 값을 입력하세요\n");
+//     printf("a의 값 :");
+//     scanf("%d",&a);
+//     printf("b의 값 :");
+//     scanf("%d",&b);
+//     printf("c의 값 :");
+//     scanf("%d",&c);
+
+//     max = a;
+//     if(b > max)
+//     {
+//         max = b;
+//     }
+//     if(c > max)
+//     {
+//         max = c;
+//     }
+//     printf("최댓값은 %d입니다",max);
+// }
