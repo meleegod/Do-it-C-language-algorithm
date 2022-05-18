f455aa5f9b5dbe50ee2cf8dcc8af84c90f7fc38e
wotjd0963 <wotjd0963@naver.com>
2022-05-19 00:53:07 +0900

    배열-1

---
+++ 2장-배열/2-1.c
@@ -0,0 +1,18 @@
+// #include <stdio.h>
+
+// #define N 7 //배열의 요소 개수
+// int main()
+// {
+//     int i;
+//     int a[N];
+//     for(i = 0; i< N; i++)
+//     {
+//         printf("a[%d] : ",i); //배열의 선언
+//         scanf("%d", &a[i]); //각 요소에 값을 입력합니다.
+//     }
+//     puts("각 요소의 값");
+//     for(i = 0; i < N; i++)
+//     {
+//         printf("a[%d] = %d\n", i, a[i]); // 각 요소의 값을 출력합니다.
+//     }
+// }
---
+++ 2장-배열/2-2.c
@@ -0,0 +1,15 @@
+// #include <stdio.h>
+
+// #define N 7 //?迭?? ??? ????
+// int main()
+// {
+//     int i;
+//     int a[6] = {1, 2, 3, 4, 5, 6};
+//     int na = sizeof(a) / sizeof(a[0]);
+//     printf("배열 a의 요소 개수는 %d입니다.\n", na);
+
+//     for(i = 0; i < na; i++)
+//     {
+//         printf("a[%d] = %d\n", i, a[i]);
+//     }
+// }
