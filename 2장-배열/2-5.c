3b3808ead08786473010880b8180b972455b237d
wotjd0963 <wotjd0963@naver.com>
2022-05-19 03:03:09 +0900

    배열-2

---
+++ 2장-배열/2-5.c
@@ -0,0 +1,33 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int maxof(const int a[], int n)
+// {
+//     int i;
+//     int max = a[0];
+//     for(i = 1; i< n; i++)
+//     {
+//         if(a[i] > max) max = a[i];
+//     }
+//     return max;
+// }
+
+// int main()
+// {
+//     int i;
+//     int *height;
+//     int number;
+//     printf("사람 수 : ");
+//     scanf("%d", &number);
+//     height = calloc(number, sizeof(int));
+//     printf("%d 사람의 키를 입력하세요.\n", number);
+//     for(i = 0; i < number; i++)
+//     {
+//         printf("height[%d] : ", i);
+//         scanf("%d", &height[i]);
+//     }
+//     printf("최댓값은 %d입니다.\n", maxof(height,number));
+//     free(height);
+
+//     return 0;
+// }
